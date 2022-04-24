5a7837622841cb0c5431a54d12cc5e806e9a814e
Jacky Wang <zwang@flexport.com>
2022-04-24 16:51:48 +0800

    Added manifest

--- src/index.tsx
+++ src/index.tsx
@@ -29,7 +29,7 @@ const App = () => {
     useEffect(() => {
         setTimeout(() => {
             setSplashVisibility(false);
-        }, Math.round(Math.random() * 1));
+        }, Math.round(Math.random() * 100));
     }, [])
 
     if (splashVisibility) {
